1b3c3b2b24cf3e798976b92b9fd1686ad602ed07
Chelsjavis <chelsea.davis975@gmail.com>
2025-09-13 15:16:51 +1000

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+        This project was coded by Chelsea D, is {""}
+        <a
+          href="https://github.com/Chelsjavis/react-week-four-homework"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        {""} and {""}
+        <a
+          href="https://gilded-selkie-97fc46.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on netlify.
+        </a>
+      </p>
     </div>
   );
 }
